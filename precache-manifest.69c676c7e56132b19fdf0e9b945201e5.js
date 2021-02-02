16fe238d25d49cbd50de3937da02919d58e61d08
Shrujana Peesari <shrujana.peesari@iths.se>
2021-02-02 16:56:06 +0100

    initial project

---
+++ precache-manifest.69c676c7e56132b19fdf0e9b945201e5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dcc2f78ae387aeb756b",
+    "url": "/css/about.c02059e6.css"
+  },
+  {
+    "revision": "60fadcc132699930c101",
+    "url": "/css/app.69edb64a.css"
+  },
+  {
+    "revision": "dc762694edbcf449441a",
+    "url": "/css/chunk-vendors.64d43e69.css"
+  },
+  {
+    "revision": "aec8be298d1a80e5a9fad3cea7e86c9e",
+    "url": "/img/image.aec8be29.jpg"
+  },
+  {
+    "revision": "599e87dac457fba71f4bfcc93d06b118",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dcc2f78ae387aeb756b",
+    "url": "/js/about.6b0f5916.js"
+  },
+  {
+    "revision": "60fadcc132699930c101",
+    "url": "/js/app.3f56c766.js"
+  },
+  {
+    "revision": "dc762694edbcf449441a",
+    "url": "/js/chunk-vendors.0f9d49d0.js"
+  },
+  {
+    "revision": "a1c68a57585f7acb8ca89d65825bab7d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
